7ab15d3dbf32e34fc3bf4baf31ad47342f75831a
RollingHog <46373241+RollingHog@users.noreply.github.com>
2023-01-02 04:08:25 +0300

    DIFFICULTY_MULTS changed

--- Visualiser.js
+++ Visualiser.js
@@ -34,19 +34,34 @@ const VARS = {
   } </style>`,
   DIFFICULTY_MULTS: [
     0,
+    // #1
     1,
-    1.2,
+    // 1.2,
+    1,
+    // #3
     2,
-    2.2,
+    // 2.2,
+    2,
+    // #5
     3,
-    3.2,
-    3.5,
+    // 3.2,
+    // 3.5,
+    3, 3,
+    // #8
     4,
-    4.3,
-    4.7,
+    // 4.3,
+    // 4.7,
+    4, 4,
+    // #11
     5,
-    5.3,
-    5.7,
+    // 5.3,
+    // 5.7,
+    5, 5,
+    // #14
+    6,
+    // 6.3,
+    // 6.6,
+    6, 6,
   ]
 }
 
@@ -279,7 +294,7 @@ const Analysis = {
       KEYWORDS.SPECIAL_TECH_COST,
       KEYWORDS.UNIT_PROPS,
       KEYWORDS.MILITARY_PARAMS,
-      ["особое"],
+      ["Слоты", "Тип отряда", "особое"],
     )
     console.table(Analysis.excludeByDict(statAllEffects, filter))
   },
