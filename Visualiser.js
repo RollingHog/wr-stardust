ae220218ba2c44a4fc62a1f43e8885b60fc152f6
RollingHog <46373241+RollingHog@users.noreply.github.com>
2023-01-04 07:28:24 +0300

    ugh

--- Visualiser.js
+++ Visualiser.js
@@ -878,7 +878,7 @@ function parseCostAndEffects(t) {
       .replace(/^(Двигатель|Скорость FTL) \+?(\d+)/, '$1:$2')
       // типы урона, эффекты оружия
       .replace(new RegExp(`^(${KEYWORDS.DAMAGE_TYPES.join('|')})$`), 'Тип урона:$1')
-      .replace(new RegExp(`^(${KEYWORDS.MODULE_PROPS.join('|')})$`), ITS_SPECIAL)
+      .replace(new RegExp(`^(${KEYWORDS.MODULE_PROPS.join('|')})$`), ALL_RIGHT)
       // эффекты, дающие великих людей
       .replace(/^\+?(\d+) велик(?:ий|их) (?:человека?)$/i, 'Великий человек:$1')
       .replace(/^\+?(\d+) велик(?:ий|их) (?:человека?)? ?(.+)?$/i, 'Великий человек ($2):$1')
