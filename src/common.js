eb7a0b26abfafb6b1fac791e9e04d863dc809b26
RollingHog <46373241+RollingHog@users.noreply.github.com>
2024-07-31 03:29:51 +0300

    BattleVisual work prototype

--- src/common.js
+++ src/common.js
@@ -67,3 +67,54 @@ function getDictKey(dictObj, rollNum) {
     i--
   }
 }
+
+function makeElDraggable(elID, headerID) {
+  const el = document.getElementById(elID)
+  const headerEl = headerID instanceof HTMLElement 
+    ? headerID
+    : document.getElementById(headerID)
+
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
+  if (headerEl) {
+    /* if present, the header is where you move the DIV from:*/
+    headerEl.onmousedown = dragMouseDown
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    el.onmousedown = dragMouseDown
+  }
+
+  el.addEventListener('click', _ => {
+    // eslint-disable-next-line no-undef
+    if(typeof HTMLUtils !== 'undefined') HTMLUtils.focusModal(el)
+  })
+
+  function dragMouseDown(e) {
+    e = e || window.event
+    e.preventDefault()
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX
+    pos4 = e.clientY
+    document.onmouseup = closeDragElement
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag
+  }
+
+  function elementDrag(e) {
+    e = e || window.event
+    e.preventDefault()
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX
+    pos2 = pos4 - e.clientY
+    pos3 = e.clientX
+    pos4 = e.clientY
+    // set the element's new position:
+    el.style.top = (el.offsetTop - pos2) + "px"
+    el.style.left = (el.offsetLeft - pos1) + "px"
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null
+    document.onmousemove = null
+  }
+}
