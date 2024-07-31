8571e84da036d67aef523f27f981a0b660b5b14f
RollingHog <46373241+RollingHog@users.noreply.github.com>
2024-07-31 03:28:47 +0300

    countPlanetMisery()

--- Visualiser.js
+++ Visualiser.js
@@ -671,6 +671,99 @@ const Analysis = {
     return +(wins / n).toFixed(3)
   },
 
+  /**
+   * @param {TGoogleDocUserObj} userObj 
+   */
+  countPlanetRawMisery(userObj) {
+    const planet = userObj.planetParams
+    const m = {
+      // unfamiliar / alien
+      u1: { unfamiliar: 1 },
+      u2: { unfamiliar: 2 },
+      u3: { unfamiliar: 3 },
+      a1: { alien: 1 },
+      a2: { alien: 2 },
+      a3: { alien: 3 },
+    }
+    const miseryTable = {
+      Вода: [
+        '1-20', m.u1, '80-100', m.u1, 
+      ],
+      Гористость: [
+        [1,5], m.u1,
+      ],
+      'Тип аномалии': [
+        '7-9', m.u1,
+      ],
+      'Масштаб аномалии': [
+        // '1-10', m.u1, '90-100', m.u1,
+      ],
+      'Расстояние до звезды': [
+        [1, 11], m.a2,
+        [2, 3, 4, 8, 9, 10], m.a1,
+        [5, 7], m.u1,
+      ],
+      'Ресурсы': [
+        '1-10', m.u1
+      ],
+      'Тип планеты': [
+        [1,2], m.a1,  
+        [3], m.u2,
+        [5], m.u1,
+        [6], m.u2,
+      ],
+    }
+    const unfamTreshold = 4
+
+    const sumMisery = {
+      unfamiliar: 0,
+      alien: 0,
+      actionsList: [],
+    }
+    for(let i in miseryTable) {
+      const value = +planet[i]
+      const b = miseryTable[i]
+      if(!value) continue
+      for(let j in b) {
+        if(j%2 === 1) continue
+        const q = b[j]
+        if (Array.isArray(q)) {
+          if(q.includes(value)) {
+            if (b[+j+1].unfamiliar) sumMisery.unfamiliar += b[+j+1].unfamiliar
+            if (b[+j+1].alien) sumMisery.alien += b[+j+1].alien
+            const obj = Object.entries(b[+j+1])
+            sumMisery.actionsList.push([i, value, obj[0][0].slice(0,1) + obj[0][1].toString(10)].join(','))
+            break
+          }
+        }
+        else if(typeof q === 'string') {
+          const baf = q.split('-')
+          if(value >= +baf[0] && value <= +baf[1]) {
+            if (b[+j+1].unfamiliar) sumMisery.unfamiliar += b[+j+1].unfamiliar
+            if (b[+j+1].alien) sumMisery.alien += b[+j+1].alien
+            const obj = Object.entries(b[+j+1])
+            sumMisery.actionsList.push([i, value, obj[0][0].slice(0,1) + obj[0][1].toString(10)].join(','))
+            break
+          }
+        }
+      }
+    }
+    if(sumMisery.unfamiliar >= unfamTreshold) {
+      sumMisery.alien += Math.floor(sumMisery.unfamiliar/unfamTreshold)
+      sumMisery.unfamiliar = sumMisery.unfamiliar % (unfamTreshold)
+    }
+    return sumMisery
+  },
+
+  // TODO move to Reports or rework
+  allPlayersMisery() {
+    const a = []
+    for(let i in window[VARS.PLAYERS_DATA_KEY]) {
+      a.push([i, this.countPlanetRawMisery(window[VARS.PLAYERS_DATA_KEY][i])])
+    }
+    console.table(Object.fromEntries(a))
+  },
+
   filterObjectByDict(obj, dict) {
     return Object.fromEntries(Object.entries(obj).filter(([key]) => dict.includes(key)))
   },
@@ -936,7 +1029,7 @@ const Analysis = {
     },
 
     // чтобы прикинуть сколько давать вкатившимся после начала игры
-    суммарная_стоимость_проектов_сделанных_каждым_из_игроков() {
+    суммарная_стоимость_проектов_игроков() {
       const result = Object.fromEntries(
         Object.entries(window[VARS.PLAYERS_DATA_KEY])
         .map(e => [e[0], {
