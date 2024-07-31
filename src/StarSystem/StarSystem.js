6cf05ad765c504d304bc9e5679c0437f44cf2aed
RollingHog <46373241+RollingHog@users.noreply.github.com>
2024-07-31 03:25:48 +0300

    planet size

--- src/StarSystem/StarSystem.js
+++ src/StarSystem/StarSystem.js
@@ -308,8 +308,10 @@ const StarSystemGenerator = {
       if(!k) continue
       
       let type = k.type
+      let size = 100
       switch (k.type) {
         case E.type.terrestrial:
+          size = 60
           if(i >= genDict.BEYOND_SNOW_LINE) type = type + '_cold'
           break
         case E.type.giant:
@@ -325,13 +327,13 @@ const StarSystemGenerator = {
       // a + b*1 = 5
       // b = 9
       // a = -4
-      el.style.left = `${8*i - 1}%`
+      el.style.left = `${8.5*i - 5}%`
       // const imgName = name
       //   .toLowerCase()
       //   .replace(/\([^)]+\)/g,'')
       //   .trim()
       //   .replace(/ /g, '_')
-      el.innerHTML = `${i}<br><img src='assets/planets/${type}.png' alt="${k.type} ${k.size}">`
+      el.innerHTML = `${i}<br><img src='assets/planets/${type}.png' style="width:${size}%" alt="${k.type} ${k.size}">`
       // el.title = name
       canvasEl.appendChild(el)
     }  
