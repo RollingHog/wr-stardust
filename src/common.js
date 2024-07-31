f0b0138c3cd2ad5b0be9d01e606facaa0b16529a
RollingHog <46373241+RollingHog@users.noreply.github.com>
2024-07-31 03:25:35 +0300

    FILL_2_TREE_TYPE extracted

--- src/common.js
+++ src/common.js
@@ -7,3 +7,11 @@ var log = console.log
 function getEl(id) {
   return document.getElementById(id)
 }
+
+var FILL_2_TREE_TYPE = {
+  "#FF9966": "Military",
+  "#CCFFCC": "Biology",
+  "#FFCC00": "Industry",
+  "#99CCFF": "Science",
+  "#CC99FF": "Sociology",
+}
